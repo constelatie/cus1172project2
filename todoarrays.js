e2afc92a526e203ed1347d69b8004a3e6b08cec2
constelatie <144731667+constelatie@users.noreply.github.com>
2026-03-06 17:52:18 -0500

    Implement pending task feature in todo list
    Added 'In Progress' button functionality for tasks.

--- todoarrays.js
+++ todoarrays.js
@@ -3,12 +3,15 @@
                     let task = document.querySelector("#task").value;
                     let taskpriority = document.querySelector("#task-priority").value;
                     const li = document.createElement('li');
-                    let task_title_text = `<span> ${ task }</span><span> ( ${taskpriority} )</span><button class="remove btn btn-dark">Remove</button><button class="complete btn btn-light">Complete</button>`
+                    let task_title_text = `<span> ${ task }</span><span> ( ${taskpriority} )</span><button class="remove btn btn-dark">Remove</button><button class="complete btn btn-light">Complete</button><button class="pending">In Progress</button>`
                     li.innerHTML = task_title_text;
                     li.querySelector(".complete").onclick = function() {
                         completeTask(li);
                         console.log("what");
                     }
+                  li.querySelector(".pending").onclick = function() {
+                    pendingTask(li);
+                  }
                     let index = taskArray.length - 1;
                     li.querySelector(".remove").onclick = function() {
                         removeTask(li, index);
@@ -38,6 +41,11 @@ function addTask(taskItem) {
 function completeTask(li) {
     li.style.textDecoration = "line-through";
     li.style.textDecorationColor = "#815858";
+}
+
+function pendingTask(li) {
+  li.style.textDecoration = "underline";
+  li.style.textDecorationColor = "#ff66b2";
 }
 
 function removeTask (li, index) {
